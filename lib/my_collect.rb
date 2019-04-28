718b76bdc3fc8144541076b8914e589b1959b130
Alex Velasquez <jtinnesco@gmail.com>
2019-04-28 05:23:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
   x = 0 
   new_collection = []
-    while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
+    while x < collection.length
+    new_collection << yield(collection[x])
+    x += 1
   end
   new_collection
 end
